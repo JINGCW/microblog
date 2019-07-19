c48d3f35ca1940964e9a2f697676003fe356a1c9
JINGCW <1562359870@qq.com>
2019-07-19 17:42:27 +0800

    version0.1

--- app/graphs/bar_line.py
+++ app/graphs/bar_line.py
@@ -1,8 +1,10 @@
 import pandas as pd
-from flask import jsonify,request
+from flask import jsonify, request
 
-from app.graphs import bp
 from app.api.errors import error_response
+from app.graphs import bp
+
+
 # import numpy as np
 
 
@@ -11,7 +13,7 @@ from app.api.errors import error_response
 
 @bp.route('/bar_line', methods=['GET', 'POST'])
 def bar_line():
-    _args=request.args
+    _args = request.args
 
     if not _args:
         return_value = [
@@ -20,21 +22,26 @@ def bar_line():
             0.004828, -0.03047185, 0.0253638, -0.03704443
         ]
 
-        return_value = pd.Series(return_value * 100).map(lambda x: round(x, 2)).tolist()
+        # return_value = pd.Series(return_value * 100).map(lambda x: round(x, 2)).tolist()
 
         emv_value = [9.26661123e+08, 9.25157511e+08, -9.47134725e+08, 8.76023947e+08,
                      8.92582803e+08, 8.92582803e+08, 8.92582803e+08, 8.42836124e+08,
                      8.46905339e+08, 8.21098570e+08, 8.41924753e+08, 8.10736130e+08]
 
+        frame = pd.DataFrame.from_dict({'return': return_value, 'emv': emv_value})
+        frame.index = pd.date_range(start='2018-01-02', periods=12, freq='D'
+                                    ).map(lambda x: x.strftime('%Y-%m-%d')).to_list()
+        frame['return'] *= 100
+        frame['return'] = frame['return'].round(2)
+        frame.drop_duplicates(inplace=True)
+
         payload = {
-            'category': pd.date_range(
-                start='2018-01-02', periods=12, freq='D'
-            ).map(lambda x: x.strftime('%Y-%m-%d')).to_list(),
-            'data': list(zip(emv_value, return_value)),
-            'bar':[min(emv_value),max(emv_value)],
-            'line':[min(return_value),max(return_value)]
+            'category': frame.index.to_list(),
+            'data': list(zip(frame.emv.to_list(), frame['return'].to_list())),
+            'bar': [min(frame.emv), max(frame.emv)],
+            'line': [min(frame['return']), max(frame['return'])]
         }
 
         return jsonify(payload)
 
-    return error_response(404,'nothing to view')
+    return error_response(404, 'to be continue...')
