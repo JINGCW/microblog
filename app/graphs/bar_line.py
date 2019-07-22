d2ce6719228d8de5e168efaf720ed7a40ccea517
JINGCW <1562359870@qq.com>
2019-07-22 16:52:09 +0800

    add vol value

--- app/graphs/bar_line.py
+++ app/graphs/bar_line.py
@@ -24,20 +24,34 @@ def bar_line():
 
         # return_value = pd.Series(return_value * 100).map(lambda x: round(x, 2)).tolist()
 
-        emv_value = [9.26661123e+08, 9.25157511e+08, 9.47134725e+08, 8.76023947e+08,
-                     8.92582803e+08, 8.92582803e+08, 8.92582803e+08, 8.42836124e+08,
-                     8.46905339e+08, 8.21098570e+08, 8.41924753e+08, 8.10736130e+08]
+        # emv_value = [9.26661123e+08, 9.25157511e+08, 9.47134725e+08, 8.76023947e+08,
+        #              8.92582803e+08, 8.92582803e+08, 8.92582803e+08, 8.42836124e+08,
+        #              8.46905339e+08, 8.21098570e+08, 8.41924753e+08, 8.10736130e+08]
 
-        frame = pd.DataFrame.from_dict({'return': return_value, 'emv': emv_value})
+        vol_value = [0.04120189895341634,
+                     0.0411296747187073,
+                     -0.04146772251867945,
+                     0.03320980525216389,
+                     0.03133448438138365,
+                     -0.029165401566960347,
+                     0.029433396631199444,
+                     0.030057759577343184,
+                     -0.027866404141429157,
+                     -0.02310100818597614,
+                     0.0362744686507009,
+                     0.029455136389356963]
+
+        frame = pd.DataFrame.from_dict({'return': return_value, 'volatility': vol_value})
         frame.index = pd.date_range(start='2018-01-02', periods=12, freq='D'
                                     ).map(lambda x: x.strftime('%Y-%m-%d')).to_list()
         # frame['return'] *= 100
         frame['return'] = frame['return'].round(2)
+        frame['volatility'] = frame['volatility'].round(2)
         frame.drop_duplicates(inplace=True)
 
         payload = {
             'category': frame.index.to_list(),
-            'data': list(zip(frame.emv.to_list(), frame['return'].to_list())),
+            'data': list(zip(frame.volatility.to_list(), frame['return'].to_list())),
             # 'bar': [min(frame.emv), max(frame.emv)],
             # 'line': [min(frame['return']), max(frame['return'])]
         }
